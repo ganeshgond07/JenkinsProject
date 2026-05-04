bbacc8b016f6614b729cf8643ba264c62e0228bd
ganeshgond07 <gondganesh987@gmail.com>
2026-05-04 16:27:28 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+class Main{
+    public static void main(String[] args){
+        System.out.println("This is java Program");
+        System.out.println("Output from java program");
+        System.out.println(new Date());
+                System.out.println("Output from program");
+    }
+}
